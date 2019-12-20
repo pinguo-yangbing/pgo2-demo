f47ade300f110a5511591fe55bec18f8694c16d9
yangbing <yangbing@camera360.com>
2019-12-20 15:51:43 +0800

    ss

--- pkg/controller/init.go
+++
@@ -1,7 +0,0 @@
-package Controller
-
-import "github.com/pinguo/pgo"
-
-func init(){
-	pgo.App.GetContainer().Bind(&TestController{})
-}
---
+++ pkg/main.go
@@ -0,0 +1,16 @@
+package main
+
+import "github.com/gin-gonic/gin"
+
+func main() {
+
+	r := gin.Default()
+	r.GET("/test/index", func(c *gin.Context) {
+		c.JSON(200, gin.H{
+			"data":"hello world",
+			"status":200,
+			"message": "pong",
+		})
+	})
+	r.Run() // listen and serve on 0.0.0.0:8080 (for windows "localhost:8080")
+}
